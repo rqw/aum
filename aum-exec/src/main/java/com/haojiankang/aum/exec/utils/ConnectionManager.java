3d8230b529ac6b041a8ee6e0a3713d3b22a92e63
renqiwei <renqiwei1220@163.com>
2018-06-15 17:41:26 +0800

    引入dbutils、oracle驱动、lombok
    Signed-off-by: 任奇炜 <renqiwei1220@163.com>

---
+++ aum-exec/src/main/java/com/haojiankang/aum/exec/utils/ConnectionManager.java
@@ -0,0 +1,20 @@
+package com.haojiankang.aum.exec.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import oracle.jdbc.OracleDriver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+@Slf4j
+public class ConnectionManager {
+    public static Connection getConnection(String driver,String url,String userName,String password) {
+        Connection conn = null;
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, userName,password);
+        } catch (Exception e) {
+
+        }
+        return conn;
+    }
+}
